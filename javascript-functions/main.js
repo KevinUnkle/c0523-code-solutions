6c450538dbdf0a4efd8140bc45ba77d3341e5e90
Kevin Unkle <kevinunk@gmail.com>
2023-05-09 23:23:23 +0000

    Completed javascript-functions exercise.

---
+++ javascript-functions/main.js
@@ -0,0 +1,32 @@
+function convertMinutesToSeconds(minutes) {
+  const seconds = minutes * 60;
+  return seconds;
+}
+console.log('5 minutes converted to seconds is:', convertMinutesToSeconds(5));
+
+function greet(name) {
+  return '"Hey, ' + name + '"';
+}
+console.log(greet('Beavis'));
+
+function getArea(width, height) {
+  return width * height;
+}
+console.log('The total area is:', getArea(17, 42));
+
+function getFirstName(person) {
+  return person.firstName;
+}
+console.log(
+  'The first name is:',
+  getFirstName({ firstName: 'Lelouche', lastName: 'Lamperouge' })
+);
+
+function getLastElement(array) {
+  const lastElement = array[3];
+  return lastElement;
+}
+console.log(
+  'The last element is:',
+  getLastElement(['propane', 'and', 'propane', 'accessories'])
+);
