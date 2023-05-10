e0df2d7d04fe568811390bcb1d6560477c5ae4bd
Kevin Unkle <kevinunk@gmail.com>
2023-05-10 21:04:34 +0000

    Completed javascript-conditionals exercise.

---
+++ javascript-conditionals/main.js
@@ -0,0 +1,97 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro,
+            recommendMovie
+ */
+function isUnderFive(number) {
+  if (number < 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isEven(number) {
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function startsWithJ(string) {
+  if (string['0'] === 'J') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrink(person) {
+  if (person >= 21) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrive(person) {
+  if (person >= 16) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrinkAndDrive(person) {
+  if (person >= 0) {
+    return false;
+  }
+}
+
+function categorizeAcidity(pH) {
+  if (pH >= 0 && pH <= 6) {
+    return 'acid';
+  } else if (pH === 7) {
+    return 'neutral';
+  } else if (pH >= 8 && pH <= 14) {
+    return 'base';
+  } else {
+    return 'invalid pH level';
+  }
+}
+
+function introduceWarnerBro(name) {
+  switch (name) {
+    case 'yakko':
+      return "We're the warner brothers!";
+    case 'wakko':
+      return "We're the warner brothers!";
+    case 'dot':
+      return "I'm cute~";
+    default:
+      console.log('Goodnight everybody!');
+  }
+}
+
+function recommendMovie(genre) {
+  switch (genre) {
+    case 'action':
+      return 'Die Hard';
+    case 'comedy':
+      return 'The Big Lebowski';
+    case 'horror':
+      return 'Halloween';
+    case 'drama':
+      return 'Titanic';
+    case 'musical':
+      return 'West Side Story';
+    case 'sci-fi':
+      return 'Alien vs. Predator';
+  }
+}
