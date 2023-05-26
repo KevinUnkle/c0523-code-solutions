fe6d18512861013ed65b42335fab93c2cacd1f14
Kevin Unkle <kevinunk@gmail.com>
2023-05-26 19:01:34 +0000

    updated variables and console logs.

--- javascript-and-json/main.js
+++ javascript-and-json/main.js
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-expressions
-[
+const books = [
   {
     isbn: '123456789',
     name: 'The Catcher in the Rye',
@@ -12,76 +11,16 @@
   },
   { isbn: '135792468', name: 'The Grapes of Wrath', author: 'John Steinbeck' },
 ];
-console.log([
-  'Book 1:',
-  {
-    isbn: '123456789',
-    name: 'The Catcher in the Rye',
-    author: 'J.D. Salinger',
-  },
-  'Book 2:',
-  { isbn: '987654321', name: 'To Kill A Mockingbird', author: 'Harper Lee' },
-  'Book 3:',
-  { isbn: '135792468', name: 'The Grapes of Wrath', author: 'John Steinbeck' },
-]);
-console.log(
-  'Type of:',
-  typeof [
-    {
-      isbn: '123456789',
-      name: 'The Catcher in the Rye',
-      author: 'J.D. Salinger',
-    },
-    { isbn: '987654321', name: 'To Kill A Mockingbird', author: 'Harper Lee' },
-    {
-      isbn: '135792468',
-      name: 'The Grapes of Wrath',
-      author: 'John Steinbeck',
-    },
-  ]
-);
-console.log(
-  JSON.stringify([
-    'Book 1:',
-    {
-      isbn: '123456789',
-      name: 'The Catcher in the Rye',
-      author: 'J.D. Salinger',
-    },
-    'Book 2:',
-    { isbn: '987654321', name: 'To Kill A Mockingbird', author: 'Harper Lee' },
-    'Book 3:',
-    {
-      isbn: '135792468',
-      name: 'The Grapes of Wrath',
-      author: 'John Steinbeck',
-    },
-  ])
-);
-console.log(
-  'Type of:',
-  typeof JSON.stringify([
-    'Book 1:',
-    {
-      isbn: '123456789',
-      name: 'The Catcher in the Rye',
-      author: 'J.D. Salinger',
-    },
-    'Book 2:',
-    { isbn: '987654321', name: 'To Kill A Mockingbird', author: 'Harper Lee' },
-    'Book 3:',
-    {
-      isbn: '135792468',
-      name: 'The Grapes of Wrath',
-      author: 'John Steinbeck',
-    },
-  ])
-);
-
-// eslint-disable-next-line no-unused-expressions
+console.log(books);
+console.log('Type of books:', typeof books);
+const jsonBooks = JSON.stringify(books);
+console.log(jsonBooks);
+console.log(typeof jsonBooks);
 
 const json = '{"Number ID": 12345, "String name": "String"}';
-console.log('Type of:', typeof JSON.stringify(json));
+console.log(json);
+console.log('type of:', typeof json);
 
-console.log(JSON.parse(json));
-console.log(typeof JSON.parse(json));
+const object = JSON.parse(json);
+console.log(object);
+console.log('type of:', typeof object);
