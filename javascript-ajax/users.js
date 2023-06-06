b8720ef8639e6aeafb4d8ab1beaa9569a6a0a6ae
Kevin Unkle <kevinunk@gmail.com>
2023-06-06 23:22:36 +0000

    Completed javascript-ajax exercise.

---
+++ javascript-ajax/users.js
@@ -0,0 +1,15 @@
+const $userList = document.querySelector('#user-list');
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+xhr.responseType = 'json';
+xhr.addEventListener('load', function () {
+  console.log(xhr.status);
+  console.log(xhr.response);
+  for (let i = 0; i < xhr.response.length; i++) {
+    const xhrResponse = xhr.response[i];
+    const $newLi = document.createElement('li');
+    $newLi.textContent = xhrResponse.name;
+    $userList.append($newLi);
+  }
+  xhr.send();
+});
