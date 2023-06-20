781564d3c1b49382b52468e709cfcbd475e323ea
Kevin Unkle <kevinunk@gmail.com>
2023-06-20 20:48:53 +0000

    Completed array-methods exercise.

---
+++ array-methods/filter.js
@@ -0,0 +1,21 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const names = [
+  'Ada',
+  'Hedy',
+  'Jean',
+  'Grace',
+  'Evelyn',
+  'Joan',
+  'Elizabeth',
+  'Janese',
+  'Donna',
+];
+
+const evens = numbers.filter((numbers) => numbers % 2 === 0);
+console.log(evens);
+
+const name = names.filter(
+  (names) => !names.includes('d') && !names.includes('D')
+);
+console.log(name);
